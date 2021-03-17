82bfa80467fcac718ef868e2dd71d9537c71051a
Marc-Philippe Huget <mphuget@gmail.com>
2021-03-17 23:29:36 +0100

    Create the add member form

--- app/src/app/pages/add-member/add-member.module.ts
+++ app/src/app/pages/add-member/add-member.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,10 +8,13 @@ import { AddMemberPageRoutingModule } from './add-member-routing.module';
 
 import { AddMemberPage } from './add-member.page';
 
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     AddMemberPageRoutingModule
   ],
--- app/src/app/pages/add-member/add-member.page.ts
+++ app/src/app/pages/add-member/add-member.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-member',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMemberPage implements OnInit {
 
-  constructor() { }
+  private member : FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+
+    this.member = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
+  
+  }
 
   ngOnInit() {
   }
